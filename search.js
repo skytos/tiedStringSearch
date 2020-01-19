528c45cd1fd7f01d232a74e90d78f8a336b16587
Skylar Broad <skylar.broad@gmail.com>
2020-01-19 15:32:10 -0800

    working coroutine search

---
+++ search.js
@@ -0,0 +1,50 @@
+function* makeConnectedString(strings) {
+  let pos = 0
+  let stringIndex = 0
+  let posInString = 0
+  let starts = [0]
+  while (stringIndex < strings.length - 1 || posInString < strings[stringIndex].length) {
+    if (posInString === strings[stringIndex].length) {
+      posInString = 0
+      stringIndex++
+      starts[stringIndex] = pos
+      continue
+    }
+    let backUp = yield strings[stringIndex][posInString]
+    if (backUp !== undefined) {
+      pos -= backUp
+      if (pos < 0) pos = 0 // don't let us back up too far
+      for (; starts[stringIndex] > pos; stringIndex--);
+      posInString = pos - starts[stringIndex] 
+    } else {
+      posInString++
+      pos++
+    }
+  }
+}
+
+function search(needle, haystack) {
+	let i = 0
+	let backUp
+	while (i < needle.length) {
+		let hay = haystack.next(backUp)
+		if (hay.done) return false
+		if (hay.value === needle[i]) {
+			i++
+			backUp = undefined
+		} else {
+			backUp = i-1
+			i = 0
+		}
+	}
+	return true
+}
+
+module.exports = {
+	makeConnectedString,
+	search
+}
+
+let s = makeConnectedString(['people call me Sk', 'y but ', 'my full n', 'ame is', ' Skylar'])
+console.log(search('Skylar', s))
+
